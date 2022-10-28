be5b04e3af353e43ee7304efbc8f85fd3480751b
Aeeeeeep <aep_sun@qq.com>
2022-10-28 22:14:29 +0800

    add aug & AdamW

--- configs/cascade_rcnn_r101_fpn_1x.py
+++ configs/cascade_rcnn_r101_fpn_1x.py
@@ -184,6 +184,17 @@ train_pipeline = [
     dict(type='LoadAnnotations', with_bbox=True),
     dict(type='Resize', img_scale=(1440, 1024), keep_ratio=True),
     dict(type='RandomFlip', flip_ratio=0.5),
+    dict(type='CopyPaste', max_num_pasted=100),
+    dict(type='Mosaic', img_scale=(1440, 1024), pad_val=114.0),
+    dict(
+        type='RandomAffine',
+        scaling_ratio_range=(0.1, 2),
+        border=(-1440 // 2, -1024 // 2)),
+    dict(
+        type='MixUp',
+        img_scale=(1440, 1024),
+        ratio_range=(0.8, 1.6),
+        pad_val=114.0),
     dict(type='Normalize', **img_norm_cfg),
     dict(type='Pad', size_divisor=32),
     dict(type='DefaultFormatBundle'),
@@ -227,7 +238,9 @@ data = dict(
         pipeline=test_pipeline))
 evaluation = dict(interval=1, metric='mAP')
 # optimizer
-optimizer = dict(type='SGD', lr=0.005, momentum=0.9, weight_decay=0.0001)
+optimizer = dict(type='AdamW', lr=6e-5, betas=(0.9, 0.999),
+                               weight_decay=0.01,
+                               paramwise_cfg=dict(custom_keys=dict(head=dict(lr_mult=10.0))))
 optimizer_config = dict(grad_clip=dict(max_norm=35, norm_type=2))
 # learning policy
 lr_config = dict(
