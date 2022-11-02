d76070ca2d3420d8fa981a7353b343b7a62a2463
aeeeeeep <aep_sun@qq.com>
2022-11-02 12:09:49 +0800

    add sahi

--- test.py
+++ test.py
@@ -1,38 +1,75 @@
 import os
 import json
-import pandas as pd
-import numpy as np
+os.getcwd()
+# import required functions, classes
+from sahi import AutoDetectionModel
+from sahi.utils.cv import read_image
+from sahi.utils.file import download_from_url
+from sahi.predict import get_prediction, get_sliced_prediction, predict
+from IPython.display import Image
 
-txt_path = './conntest'
-Width = 2880
-Height = 2048
-sub_img_row = 2
-sub_img_col = 2
-overlap = 0.3
-images = os.listdir('./test_images')
+# download cascade mask rcnn model&config
+model_path = 'work_dirs/cascade_convnext_s/epoch_48.pth'
+config_path = 'configs/cascade_rcnn_r50_fpn_1x.py'
+
+# ## 4. Batch Prediction
+model_type = "mmdet"
+model_path = model_path
+model_config_path = config_path
+model_device = "cuda:0" # or 'cuda:0'
+model_confidence_threshold = 0.001
+
+slice_height = 768
+slice_width = 960
+overlap_height_ratio = 0.2
+overlap_width_ratio = 0.2
+
+source_image_dir = "data/test_images"
+
+detection_model = AutoDetectionModel.from_pretrained(
+    model_type='mmdet',
+    model_path=model_path,
+    config_path=config_path,
+    confidence_threshold=0.001,
+    # image_size=(2880,2048),
+    device="cuda:0", # or 'cuda:0'
+)
 jdict = []
-for img_path in images:
-    path = os.path.join(txt_path, img_path.split('.')[0] + '.txt')
-    try:
-        txt = pd.read_table(path, header=None, sep=' ')
-        txt.loc[:, [1, 2]], txt.loc[:, [3, 4]] = (txt.loc[:, [1, 2]].values) * np.array([Width,Height]), (txt.loc[:, [3,4]].values) * np.array([Width, Height])
-        image_id = img_path.split('.')[0]
-        ids = txt[0].tolist()
-        box = np.array(txt.loc[:,1:4])
-        box[:,:2] -= box[:, 2:] / 2
-        # box = np.where(box<0, 0, box)
-        scores = np.array(txt.loc[:,5])
-        for p, b, c in zip(scores.tolist(), box.tolist(), ids):
-            jdict.append({'image_id': image_id,
-                          'category_id': round(c),
-                          'bbox': [round(x, 3) for x in b],
-                          'score': round(p, 5)})
-    except:
-        pass
 
-pred_json = '/home/pacaep/Downloads/pred.json'
-print('\nSaving %s...' % pred_json)
+fp = open('data/test.txt','r')
+test_list = fp.readlines()
+imgs = []
+
+for test_1 in test_list:
+    test_1 = test_1.replace('\n','')
+    name = test_1
+    imgs.append(name)
+count = 0
+
+for img in imgs:
+    count += 1
+    print('model is processing the {}/{} images.'.format(count,len(imgs)))
+    result = get_sliced_prediction(
+        img,
+        detection_model,
+        slice_height = slice_height,
+        slice_width = slice_width,
+        overlap_height_ratio = 0.2,
+        overlap_width_ratio = 0.2
+    )
+
+    rawjson = result.to_coco_predictions(image_id=1)
+    for item in rawjson:
+        bbox = item['bbox']
+        float_bbox = []
+        for i in bbox:
+            float_bbox.append(float(i)+0.00001)
+        jdict.append({'image_id':img[29:-4],
+                      'category_id':item['category_id'],
+                      'bbox':float_bbox,
+                      'score':round(item['score'],5)})
+
+pred_json = './pred.json'
+print('\nSaveing %s...' % pred_json)
 with open(pred_json, 'w') as f:
     json.dump(jdict, f)
-
-
